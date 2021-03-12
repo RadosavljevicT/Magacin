8ab5745b0a1447fcd485dcafa247424b626d1f72
tamararadosavljevic <radosavljevic.tamara97@gmail.com>
2021-03-12 22:54:16 +0100

    Napravljena klasa RadSaArtiklima koja implementira MagacinInterfejs

---
+++ src/Artikli/RadSaArtiklima.java
@@ -0,0 +1,64 @@
+package Artikli;
+
+import KancelarijskiMaterijali.KancelarijskiMaterijal;
+import Knjige.Knjiga;
+import KucnaHemija.KucnaHemija;
+import specifikacijainterfejsa.MagacinInterfejs;
+
+public class RadSaArtiklima implements MagacinInterfejs {
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dodajKancelarijskiMaterijal(KancelarijskiMaterijal kancelarijskiMaterijal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dodajKucnuHemiju(KucnaHemija kucnaHemija) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciKancelarijskiMaterijal(KancelarijskiMaterijal kancelarijskiMaterijal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciKucnuHemiju(KucnaHemija kucnaHemija) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int pronadjiKnjigu(int sifra) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int pronadjiKancelarijskiMaterijal(int sifra) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int pronadjiKucnuHemiju(int sifra) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
